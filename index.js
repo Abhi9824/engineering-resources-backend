d8a6e0a6162d2413a34ae7342d50d6869a4512e1
Abhi9824 <abhijitchanda2000@gmail.com>
2025-06-14 20:11:16 +0530

    fix:fixed cors

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 const corsOptions = {
-  origin: "https://manage-x-app.vercel.app/",
+  origin: "https://manage-x-app.vercel.app",
   // credentials: true,
 };
 app.use(cors(corsOptions));
