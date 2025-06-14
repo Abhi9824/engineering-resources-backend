70ca7ff4577488c06f94502261aae9120ed4506f
Abhi9824 <abhijitchanda2000@gmail.com>
2025-06-14 20:05:29 +0530

    feat:added frontend live url

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://manage-x-app.vercel.app/",
   // credentials: true,
 };
 app.use(cors(corsOptions));
